53237e068ad30914c572f08eaa5457c80b25da61
royyanfathullah <156770481+royyanfathullah@users.noreply.github.com>
2024-02-19 10:40:51 +0700

    menambahkan list angka di index.php

--- index.php
+++ index.php
@@ -8,5 +8,12 @@
 <body>
   <h1>hello world!</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam repudiandae fugit dignissimos modi aliquid ullam tempore voluptas aut eligendi quas tempora, minus autem explicabo suscipit ipsam est dolorum quam esse.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+    <li>tiga</li>
+    <li>empat</li>
+    <li>lima</li>
+  </ul>
 </body>
 </html>
